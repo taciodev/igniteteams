8cfb1dc6c5e2546b6db10f933338479cdba5ca9c
taciano <tacianohora2020@hotmail.com>
2023-02-09 01:20:39 -0300

    feat: estrutura das pastas e aplicação da estratégia Path Mapping nas importações

---
+++ src/screens/Groups/index.tsx
@@ -0,0 +1,18 @@
+import { View, Text, StyleSheet } from "react-native";
+
+export function Groups() {
+  return (
+    <View style={styles.container}>
+      <Text>Hello World!</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
